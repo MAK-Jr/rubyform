8e4b8f8381214bc157f4f1eff4dda9e8742dc6cb
Pitt Mak <Skeleton.MAK.Jr@gmail.com>
2013-06-08 16:28:17 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyform::Application.config.secret_token = '78fef6686d7c9e1d0c3ff72bf8d2bd2d7e771e8c936c965ccacbf2baa13a82e3b21921e8e80429dc9416e9922c7fa73c4304c38cee4b6e8d0f85762d47ab5235'
